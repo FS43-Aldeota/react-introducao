9e9208965d479253a27f024328c95cf59c68d8b7
Francisco Gleidson Teixeira Rocha <Francisco.Rocha@digitalcollege.internal>
2025-05-02 11:54:35 -0300

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import BemVindo from "./BemVindo";
 import Bolo from "./Bolo";
 import Carro from "./Carro";
+import Contador from "./Contador";
+import Interruptor from "./Interruptor";
+import Menu from "./Menu";
 import Musico from "./Musico";
 
 const App = () => {
   return (
     <>
-      <h1>Olá mundo</h1>
+      {/* <h1>Olá mundo</h1>
       <BemVindo nome="Ailana" cidade="Aquiraz"/>
       <BemVindo nome="André" cidade="Fortaleza"/>
       <BemVindo nome="Marcio" cidade="Fortaleza"/>
       <BemVindo nome="Victor" cidade="Fortaleza"/>
       <BemVindo nome="Ieda" cidade="Maracanaú"/>
-
       <Bolo sabor={"cenoura"} cobertura={"chocolate"} /> 
-
       <Carro modelo={"Civic"} marca={"Honda"} />
       <Carro modelo={"Moby"} marca={"Fiat"} />
-
       <Musico instrumento={"bateria"} banda={"offspring"} />
-      <Musico instrumento={"guitarra"} banda={"calcinha preta"} />
+      <Musico instrumento={"guitarra"} banda={"calcinha preta"} /> */}
+      {/* <Interruptor />
+      <Contador /> */}
+      <Menu />
     </>
   );
 }
